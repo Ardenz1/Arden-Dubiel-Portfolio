5c4af2f6404bebcb93987a40cd665fc5cd20bddb
arden <ardenbyui.edu>
2024-02-16 16:21:41 -0700

    recentChanges

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ const d = new Date();
 let year = d.getFullYear();
 
 document.querySelector('#year').innerHTML = year; 
-console.log("hi")
+
+// Hamburger Menu 
+const mainnav = document.querySelector('.navUl')
+const hambutton = document.querySelector('#menu')
+const hr = document.querySelector('hr')
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show')
+  hambutton.classList.toggle('show')
+});
